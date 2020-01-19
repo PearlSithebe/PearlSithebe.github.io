e4bea2cffc2782262ff92b64de06f45430879317
PearlSithebe <pearlsithebe@gmail..com>
2020-01-20 00:30:36 +0200

    Added React to the Website

---
+++ src/components/contact.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+
+
+class Contact extends Component {
+  render() {
+    return(
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Palesa Pearl Sithebe</h2>
+            <img
+              src="https://i.ibb.co/gdx9X3K/56197363-486745525193665-6552733031039516854-n.jpg"
+              alt="avatar"
+              style={{height: '250px'}}
+               />
+             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lets keep in touch</p>
+
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr/>
+
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fab fa-linkedin"/>
+                    LinkedIn : Palesa Sithebe
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fas fa-envelope"/>
+                    Email : pearlsithebe@gmail.com
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fab fa-github"/>
+                    Github : Pearlsithebe
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fas fa-mobile" />
+                    Number : 0761933487
+                  </ListItemContent>
+                </ListItem>
+
+
+              </List>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Contact;
